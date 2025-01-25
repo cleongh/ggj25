08f258ab1b42fbe8f7ba4afa801fbc331e99065d
Ana Ruiz <ana.ruiz@fidesmo.com>
2025-01-25 15:09:12 +0100

    Load character spritesheets and create animations on boot

--- src/boot.js
+++ src/boot.js
@@ -6,6 +6,18 @@ import relationships from "../assets/tokens/relationships.png"
 import work from "../assets/tokens/work.png"
 
 /**
+ * List of character names. We are assuming that character spritesheets are: 
+ * - 4 128x128 frames
+ * - named <character name>.png
+ * - stored under /assets/characters
+ *
+ * Then, on boot.js, we will: 
+ * - load their spritesheets on `preload()`
+ * - create the idle animation for each caracter on `create()`. This animation will be named "idle_<character name>"
+ */
+let characterNames = ["mrbuble", "mrbatpat", "mrmagoo"]
+
+/**
  * Escena para la precarga de los assets que se usarán en el juego.
  * Esta escena se puede mejorar añadiendo una imagen del juego y una 
  * barra de progreso de carga de los assets
@@ -31,6 +43,14 @@ export default class Boot extends Phaser.Scene {
     this.load.image("physical", physical)
     this.load.image("relationships", relationships)
     this.load.image("work", work)
+
+    // Load character spritesheets
+    this.load.path = 'ggj25/assets/characters/';
+    characterNames.forEach((enemyName) => {
+      console.log("Loading png for ", enemyName)
+      this.load.spritesheet(enemyName, enemyName + ".png", { frameWidth: 128, frameHeight: 128 })
+    })
+
   }
 
   /**
@@ -40,6 +60,18 @@ export default class Boot extends Phaser.Scene {
   create() {
     // this.scene.start('card-combat');
     // this.scene.start('main-menu');
-    this.scene.start('map');
+
+    // Create character idle animations
+    characterNames.forEach((enemyName) => {
+      console.log("creating idle animation for enemy ", enemyName)
+      this.anims.create({
+        key: 'idle_' + enemyName,
+        frames: this.anims.generateFrameNumbers(enemyName, { start: 0, end: 3 }),
+        frameRate: 6,
+        repeat: -1
+      })
+    })
+
+    this.scene.start('test-scene');
   }
 }
