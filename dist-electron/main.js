5a7b7e01a2abea16e78c327c72f6ba69f2175161
Carlos León <cleon@ucm.es>
2025-01-27 12:47:43 +0100

    casi electron

--- dist-electron/main.js
+++
@@ -1,9 +0,0 @@
-import { app, BrowserWindow } from "electron";
-app.whenReady().then(() => {
-  const win = new BrowserWindow();
-  win.setMenu(null);
-  console.log("ates");
-  win.loadFile("index.electron.html");
-  console.log("vale");
-  win.webContents.openDevTools();
-});
