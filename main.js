49d2867ea2df3bdcd2f74ad1c3abdac33d5a6d29
Carlos León <cleon@ucm.es>
2025-01-27 13:29:08 +0100

    electron parece funcionar

--- main.js
+++ main.js
@@ -1,13 +1,16 @@
 import { app, BrowserWindow } from 'electron';
 import * as path from 'path';
+// import html from './index.html?url'
 
 let mainWindow;
 
 function createWindow() {
     mainWindow = new BrowserWindow({});
-
+    mainWindow.menuBarVisible
+    mainWindow.setMenu(null);
     // Vite dev server URL
-    mainWindow.loadURL('http://localhost:5173');
+    // mainWindow.loadURL('http://localhost:5173');
+    mainWindow.loadFile("out/renderer/index.electron.html");
     mainWindow.on('closed', () => mainWindow = null);
 }
 
